c5cfdc523836dd24839f367747e542e3cc7de54e
nana abeka otoo <abekaotoo@gmail.com>
2025-02-22 17:50:12 +0000

    updated the __init__.py for the nafes package with the transform.py script

--- nafes/__init__.py
+++ nafes/__init__.py
@@ -1,10 +1,10 @@
-"""Top-level package for sofes."""
+"""Top-level package for nafes."""
 
 __author__ = """Nana Abeka Otoo"""
 __email__ = 'abekaotoo@gmail.com'
 __version__ = '0.0.3'
 
-
+from .transform import *
 from .dataset import *
 from .mutate_labels import *
 from .mutated_validation import *
